39b6587f7a5bbf0bc1ca7391fbff54a262f0cb26
AsaGick <asagicks@gmail.com>
2024-04-07 21:28:52 +0330

    update depen

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asagick/portfolio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '80526ae108a948c667a96279e75b8e0817ed719c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'asagick/portfolio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '80526ae108a948c667a96279e75b8e0817ed719c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
